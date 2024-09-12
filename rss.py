846c708db5cd294446815eba1d64deb856b7ca9d
Aleksandr K <63010958+Alivkon@users.noreply.github.com>
2024-09-12 23:07:41 +0200

    Update rss.py
    Translate comments to engish

--- rss.py
+++ rss.py
@@ -1,43 +1,37 @@
 import feedparser
 from aiogram import Bot, Dispatcher, executor, types
-
-# Параметры для Telegram
 from key import TELEGRAM_TOKEN
 import logging
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
-# Параметры для Telegram
 
-CHAT_ID = '1232578036' # ID вашего чата
+CHAT_ID = '123456789' # ID вашего чата
 
 Bot = Bot(TELEGRAM_TOKEN)
 dp = Dispatcher(Bot)
 
-
-# Функция для получения новостей из RSS-ленты
+# A function for getting news from an RSS feed
 def get_news():
     feed_url = 'https://lenta.ru/rss/last24'
     feed = feedparser.parse(feed_url)
     news_items = []
-    for entry in feed.entries[:1]:  # Получаем 1 последнюю новость
-        #news_items.append(f"{entry.title}\n{entry.link}\n")
+    for entry in feed.entries[:1]:  # We receive 1 latest news. Replace 1 with the required number of news items
         news_items.append(f"{entry.title}\n")
     print (news_items)
     return '\n'.join(news_items)
 
-# Обработчик команды /start
+# The handler of the command /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Привет! Я бот, который отправляет последнюю новость портала Lenta.ru. Для получения новости наберите команду /news")
+    await message.reply("Hi! I am a bot that sends the latest portal news Lenta.ru . To receive the news, type the command /news")
 
-# Обработчик команды /news
+# The handler of the command /news
 @dp.message_handler(commands=['news'])
 async def send_news(message: types.Message):
     news = get_news()
     await Bot.send_message(chat_id=CHAT_ID, text=news)
 
-# Главная функция
 def main():
     # Запуск бота
     executor.start_polling(dp, skip_updates=True)
